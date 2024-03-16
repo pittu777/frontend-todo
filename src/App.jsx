7843138e33dd2a5f48e56ac88245b8d61eb6a73f
pittu777 <pittuprasanth98@gmail.com>
2024-03-16 21:12:43 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
+// import './App.css'
+
+// import ToDo from './components/ToDo'
+// import { getAllToDo, addToDo, updateToDo, deleteToDo } from './utils/HandleApi'
+
+// function App() {
+//     const [todo, setToDo] = useState([])
+//     const [text, setText] = useState("")
+//     const [toDoId, setToDoId] = useState("")
+//     const [isUpdating, setIsUpdating] = useState(false);
+//     useEffect(() => {
+//         getAllToDo(setToDo)
+
+//     }, [])
+
+//     const updateMode=(_id,text)=>{
+//         setIsUpdating(true)
+//         setText(text)
+//         setToDoId(_id)
+
+//     }
+
+//     return (
+//         <div className='App'>
+//             <div className="container">
+//                 <h1>ToDo App</h1>
+//                 <div className="top">
+//                     <input type="text" placeholder='add todos..' value={text} onChange={(e) => setText(e.target.value)} />
+//                     <div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
+//                         {isUpdating ? "update" : "Add"}
+//                     </div>
+
+//                 </div>
+//                 <div className="list">
+//                     {todo.map((item) => {
+//                         return <ToDo key={item._id} 
+//                         text={item.text}
+//                         updateMode={()=>updateMode(item._id, item.text)} 
+//                         deleteToDo={()=>deleteToDo(item._id, setToDo)}
+//                         />
+//                     })}
+
+//                 </div>
+//             </div>
+
+//         </div>
+//     )
+// }
+
+// export default App
+
+
+
+import React, { useEffect, useState } from 'react'
 import './App.css'
-
 import ToDo from './components/ToDo'
 import { getAllToDo, addToDo, updateToDo, deleteToDo } from './utils/HandleApi'
 
@@ -12,14 +65,25 @@ function App() {
     const [isUpdating, setIsUpdating] = useState(false);
     useEffect(() => {
         getAllToDo(setToDo)
-
     }, [])
 
-    const updateMode=(_id,text)=>{
+    const updateMode = (_id, text) => {
         setIsUpdating(true)
         setText(text)
         setToDoId(_id)
+    }
 
+    const handleAddToDo = () => {
+        if (text.trim() !== "") { // Check if the input is not empty
+            if (isUpdating) {
+                updateToDo(toDoId, text, setToDo, setText, setIsUpdating)
+            } else {
+                addToDo(text, setText, setToDo)
+            }
+        } else {
+            // Show message if input is empty
+            alert("Please enter something")
+        }
     }
 
     return (
@@ -28,23 +92,20 @@ function App() {
                 <h1>ToDo App</h1>
                 <div className="top">
                     <input type="text" placeholder='add todos..' value={text} onChange={(e) => setText(e.target.value)} />
-                    <div className="add" onClick={isUpdating ? () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) : () => addToDo(text, setText, setToDo)}>
-                        {isUpdating ? "update" : "Add"}
+                    <div className="add" onClick={handleAddToDo}>
+                        {isUpdating ? "Update" : "Add"}
                     </div>
-
                 </div>
                 <div className="list">
                     {todo.map((item) => {
-                        return <ToDo key={item._id} 
-                        text={item.text}
-                        updateMode={()=>updateMode(item._id, item.text)} 
-                        deleteToDo={()=>deleteToDo(item._id, setToDo)}
+                        return <ToDo key={item._id}
+                            text={item.text}
+                            updateMode={() => updateMode(item._id, item.text)}
+                            deleteToDo={() => deleteToDo(item._id, setToDo)}
                         />
                     })}
-
                 </div>
             </div>
-
         </div>
     )
 }
